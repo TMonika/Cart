98ca01732faefb24340237ac169c62484defcac6
TMonika <monika.krishna436@gmail.com>
2015-12-09 19:39:44 +0530

    Checking by adding logs

--- replacestring.js
+++ replacestring.js
@@ -2,15 +2,19 @@ var censoredWords = ['sad', 'bad', 'mad'];
 var customCensoredWords = [];
 function censor (string) {
 	for (index in censoredWords) {
+		console.log("WORD": censoredWords[index]);
 		string = string.replace(censoredWords[index], "REPLACED_CENSOREDWORDS");
 	}
 	for (index in customCensoredWords) {
+		console.log("CUSTOM WORD": customCensoredWords[index]);
 		string = string.replace(customCensoredWords[index], "REPLACED_CUSTOMCENSOREDWORDS");
 	}
+	console.log("STRING", string);
 	return string;
 }
 
 function addCensoredWords (word) {
+	console.log("ADDITION WORD", word);
 	customCensoredWords.push(word);
 }
 
